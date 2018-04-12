3f31d0fed666a6156fb23def17b26d86c23a41f7
Nikolai Bystrov <isderevni@gmail.com>
2018-04-12 14:31:43 +0300

    fixed gitignore + hibernate

--- src/main/java/com/abra/university/databases/ExampleEntity.java
+++ src/main/java/com/abra/university/databases/ExampleEntity.java
@@ -1,5 +1,28 @@
 package com.abra.university.databases;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class ExampleEntity {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
 }
--- src/main/java/com/abra/university/databases/HibernateUtil.java
+++ src/main/java/com/abra/university/databases/HibernateUtil.java
@@ -1,5 +1,44 @@
 package com.abra.university.databases;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.internal.SessionFactoryImpl;
+
 public class HibernateUtil {
 
+  private static final String PERSISTENT_UNIT_NAME = "universitydb";
+
+//  private static final EntityManagerFactory emf;
+//
+//  static {
+//    try {
+//      emf = Persistence.createEntityManagerFactory(PERSISTENT_UNIT_NAME);
+//    } catch (Throwable ex) {
+//      throw new ExceptionInInitializerError(ex);
+//    }
+//  }
+//
+//  public static EntityManager getEm() {
+//    return emf.createEntityManager();
+//  }
+
+  public static void main(String[] args) {
+    SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml")
+        .buildSessionFactory();
+    Session session = sessionFactory.openSession();
+    session.beginTransaction();
+
+    ExampleEntity exampleEntity = ExampleEntity.builder().name("namich").build();
+
+    session.persist(exampleEntity);
+
+    session.getTransaction().commit();
+    session.close();
+    System.out.println("HibernateUtil.main");
+    sessionFactory.close();
+  }
 }
